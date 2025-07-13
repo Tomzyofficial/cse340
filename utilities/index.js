e733b877a53caefe0993eeaf0df4ae64f244602b
Tomzyofficial <tomzyofficial54@gmail.com>
2025-07-13 19:29:57 +0100

    Remove trailing slash on img

--- utilities/index.js
+++ utilities/index.js
@@ -43,9 +43,9 @@ Util.buildClassificationGrid = async function(data){
         + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
         + ' details"><img src="' + vehicle.inv_thumbnail 
         +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-        +' on CSE Motors" /></a>'
+        +' on CSE Motors"></a>'
         grid += '<div class="namePrice">'
-        grid += '<hr />'
+        grid += '<hr>'
         grid += '<h2>'
         grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
         + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
@@ -76,7 +76,7 @@ Util.buildInventoryDetail = async function (vehicle) {
     }
    
     detail = '<div id="container">'
-    detail += '<img src="' + vehicle.inv_image + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model + '" />'
+    detail += '<img src="' + vehicle.inv_image + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model + '">'
     detail += '<div>'
     detail += '<p>Make: ' + vehicle.inv_make + '</p>'
     detail += '<p>Model: ' + vehicle.inv_model + '</p>'
